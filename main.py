21749f1974f9f04865bb14fc34ce339b01227e6d
Matveev Ilia <i.matveev@studionx.ru>
2019-09-07 14:24:07 +0300

    Added `getRandomizedBoard` and `generateRevealedBoxesData` methods

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Enjoiy!
 #   Import block
 
 import pygame
+import numpy as np
 
 
 #   Initializing important hyperparameters
@@ -56,4 +57,78 @@ OVAL = 'oval'
 
 ALLCOLORS = (RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN)
 ALLSHAPES = (DONUT, SQUARE, DIAMOND, LINES, OVAL)
+
+
+#   Okay, let's rock
+
+def main():
+
+    """
+
+    Main function to call
+
+    :return:
+    """
+
+    global FPSCLOCK, DISPLAYSURF                                        # initializing  global variables
+
+    pygame.init()
+
+    FPSCLOCK = pygame.time.Clock()                                      # creating a clock object to operate fewer
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))  # creating a display object
+
+    mainBoard = getRandomizedBoard()                                    # generating distribution of objects
+    revealedBoxes = generateRevealedBoxesData(False)                    # set object's visibility status
+
+
+def getRandomizedBoard():
+
+    """
+
+    Description
+    ____
+
+
+    Creates a NxM matrix filled with object to interact with during the game – colored squares with different figures
+
+
+    Return
+    ____
+
+    * result: array-like | NxM matrix with game objects
+
+    """
+
+    n_unique_combinations = int(BOARDHEIGHT * BOARDWIDTH / 2)
+
+    combinations_array = np.vstack(np.transpose(np.meshgrid(ALLCOLORS, ALLSHAPES)))
+    np.random.shuffle(combinations_array)
+    combinations_array = combinations_array[: n_unique_combinations]
+    combinations_array = np.vstack((combinations_array, combinations_array))
+    np.random.shuffle(combinations_array)
+
+    result_matrix = np.split(combinations_array, BOARDWIDTH)
+
+    return result_matrix
+
+
+def generateRevealedBoxesData(bool_val):
+
+    """
+
+    Description
+    ____
+
+
+    Generate an array represents which blocks are covered
+
+
+    Return
+    ____
+
+    * result: array-like | NxM matrix
+
+    """
+
+    return np.full((BOARDWIDTH, BOARDHEIGHT), bool_val)
 
